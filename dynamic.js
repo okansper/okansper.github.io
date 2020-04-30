917df4c1bcd7271985e2a0b33d23c8e997261f0c
Olivia <oliviakansperling@gmail.com>
2020-04-30 14:35:48 -0400

    fixed positions

--- dynamic.js
+++ dynamic.js
@@ -58,6 +58,7 @@ function expandWorkObj(obj_id){
     var longdescript = longDescriptMap.get(obj_id);
 
     var picdescript = picDescriptMap.get(obj_id);
+    console.log(picdescript);
     var descriptformat = descriptformatMap.get(obj_id);
     document.getElementById('expandedobj').style.opacity='1.0';
     document.getElementById('expandedphotos').style.opacity='1.0';
@@ -109,17 +110,17 @@ const picDescriptMap = new Map();
 const descriptformatMap = new Map();
 shortDescriptMap.set('frisatsun', 'December 2017 <br> <i>Fri/Sat/Sun (Material Girl in a 3D world)</i>, a VR Play<br><div class="workdescript"><img src="index_files/fri.png" id="fripic" style="float:left;padding-right:5px;width:150px;height:100px;"> An interactive, immersive narrative piece created for Brown University\'s <a class="mylink" href="http://writing3d.xyz/#Cave%20Writing">CAVE VR space</a>. A study of mood and atmosphere, intimate places and interior dialogues through sound, language, and image.  <button type="button" onclick=\'expandWorkObj("frisatsun", "fripic")\'>SEE MORE...</button></div>');
 longDescriptMap.set('frisatsun', 'December 2017. <i>Fri/Sat/Sun (Material Girl in a 3D world)</i>, a VR Play<br><br>An interactive, immersive narrative piece created for Brown University\'s <a class="mylink" href="http://writing3d.xyz/#Cave%20Writing">CAVE VR space</a>. A study of mood and atmosphere, intimate places and interior dialogues through sound, language, and image. <br> <br> The ways in which texts such as music, movies, material objects, and critical theory mold experience are interrogated over the course of a fun weekend! The girls(?) in Fri/Sat/Sun both seek to process their lives through this web of texts/media, and struggle with ideas of selfhood and emotional “reality” in the face of this mediation. Here, the material takes center stage; differences between the “characters” and the “scenery” are elided, and dialogue, in a form reminiscent of the language of digital communication, is “texted” by both people and their props. The piece tries to find a form of theater commensurate to digital language and virtual experience. In taking seriously and taking pleasure in things, Fri/Sat/Sun is also interested in questioning the idea of human selfhood as independent from the materials and media that surround us.');
-picDescriptMap.set('frisatsun', '<img src="index_files/fri.png" id="fripic" style="position:absolute; left:25px; top:25px;width:450px;height:300px;">'+
-        '<img src="index_files/sat.png" style="position:absolute; left:25px; top:350px;width:450px;height:300px;">'+
-        '<img src="index_files/sun.png" style="position:absolute; left:500px; top:25px;width:450px;height:300px;">');
+picDescriptMap.set('frisatsun', '<img src="index_files/fri.png" id="fripic" style="position:fixed; left:25px; top:25px;width:450px;height:300px;">'+
+        '<img src="index_files/sat.png" style="position:fixed; left:25px; top:350px;width:450px;height:300px;">'+
+        '<img src="index_files/sun.png" style="position:fixed; left:500px; top:25px;width:450px;height:300px;">');
 descriptformatMap.set('frisatsun', 'position:fixed; left:510px; top:350px; background-color: #gray; z-index:10; padding-right: 25px');
 
 shortDescriptMap.set('courtdebt', "April 2019. <i>The College Hill Independent</i>: Data journalism on court debt in Rhode Island <br> <img class=\"workpic\" src=\"index_files/courtdata.jpg\"> With a team of journalists investigating the cycle of court debt in RI, I wrote a program to scrape and crunch data from files from the RI Judiciary's case portal. The piece won a regional collegiate <a class='mylink' href='https://rinewstoday.com/college-journalists-win-spj-awards/'>award</a> for in-depth reporting. <button type='button' onclick='expandWorkObj('courtdebt')'>SEE MORE</button>");
 longDescriptMap.set('courtdebt', "April 2019. <i>The College Hill Independent</i>: Data journalism.<br> <br> With a team of journalists investigating the cycle of court debt in RI, I wrote a program to scrape and crunch data from files from the RI Judiciary's case portal. Statistics such as percentage of Rhode Islanders in debt to the judiciary and total debt to courts were previously unavailable to the public. The project was praised by journalists and local activist groups. View all data, read about data collection method, and download my Java program <a class='mylink' href='https://drive.google.com/drive/folders/10iylFx--L8zDoUufrE1KHlPUK04ia4jG'>here</a>. Read article <a class='mylink' href='https://www.theindy.org/1739'>here</a>");
-picDescriptMap.set('courtdebt', "<img src='index_files/debt_ability.jpg' style='position:absolute; left:25px; top:25px;width:450px;height:300px;'\>"+
-        '<img src="index_files/courtdata.jpg" style="position:absolute; left:25px; top:350px;width:450px;height:300px;opacity:1.0;">'+
-        '<img src="index_files/debt_collection.png" style="position:absolute; left:500px; top:25px;width:350px;height:180px;">'+
-        '<img src="index_files/debt_percent.png" style="position:absolute; left:500px; top:240px;width:240px;height:170px;">')
+picDescriptMap.set('courtdebt', "<img src='index_files/debt_ability.jpg' style='position:fixed; left:25px; top:25px;width:450px;height:300px;'\>"+
+        '<img src="index_files/courtdata.jpg" style="position:fixed; left:25px; top:350px;width:450px;height:300px;opacity:1.0;">'+
+        '<img src="index_files/debt_collection.png" style="position:fixed; left:500px; top:25px;width:350px;height:180px;">'+
+        '<img src="index_files/debt_percent.png" style="position:fixed; left:500px; top:240px;width:240px;height:170px;">')
 descriptformatMap.set('courtdebt', 'position:fixed; left:510px; top:450px; width:450px; height:300px;')
 
 /*shortDescriptMap.set('mermaid', 'December 2016 <br> Digital Performance class: <i>Mermaid/Dark Night</i><br><div class="workdescript"><img src="index_files/mermaidpic.jpg" style="float:left;padding-right:5px;width:150px;height:100px;">Installation/performance project/opera: ideo created from found Youtube footage of GoPros swept away from their owners and into the waves --> found by mermaid and used to create deep sea creature panorama. With Marcel Zaes (music composition) and Maurice Decaul (music text).<button type="button" onclick="expandWorkObj("mermaid")">WATCH HERE</button></div>');
